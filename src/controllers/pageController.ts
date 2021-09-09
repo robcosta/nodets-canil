3df1ac6032339a8f7b3b8927b10502ab08175d3e
Roberto Costa <robertocosta66@gmail.com>
2021-09-09 11:41:09 -0300

    preparing the views

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,15 +1,17 @@
 import { Request, Response } from 'express';
 
 export const home = (req: Request, res: Response) => {
-    res.send('Home no controller');
-    //res.render('pages/page')
+    res.render('pages/page')
 };
 export const dogs = (req: Request, res: Response) => {
+    res.send('Dogs');
     //res.render('pages/dogs')
 };
 export const cats = (req: Request, res: Response) => {
+    res.send('Cats');
     //res.render('pages/cats')
 };
 export const fishes = (req: Request, res: Response) => {
+    res.send('Fishes');
     //res.render('pages/fishes')
 };
